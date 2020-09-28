6e0953e4ef77af87c0d032ae6eb76e0c5a7be26c
theo-glitch <58421051+theo-glitch@users.noreply.github.com>
2020-09-28 16:42:33 +0200

    reinitialisation

---
+++ E4/SmartEDU Free Website Template - Free-CSS.com/smartedu/Template/reinitialisation.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Confirmation mot de passe oublié</title> <!--on nomme le titre de la page-->
+	<meta charset="UTF-8"> <!--On encode en utf-8-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="../lib/images/icons/logo.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../lib/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../lib/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
+
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../lib/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../lib/css/main.css">
+<!--===============================================================================================-->
+	<!--On importe les bibliothèques de bootsrap néscéssaires au design dans le vendor, on importe les feuilles css...-->
+</head>
+<body>
+
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form-title" style="background-image: url(../lib/images/logo.png);">
+					<span class="login100-form-title-1">
+						Vous recevrez un mail avec votre mot de passe de secours dans quelques instant
+					</span>
+				</div>
+
+
+					<div class="flex-sb-m w-full p-b-30">
+						<div class="contact100-form-checkbox">
+                                                            <!--on met de l'espace dans le cadre-->
+
+						</div>
+
+
+					</div>
+<br>
+<br>
+						<center><button class="login100-form-btn" >
+							<a href="formulaire_connexion_etudiant.php"> Connexion</a>
+						</button></center> <!--On met un bouton dans lequel on met un lien qui retourne vers la page de connexion -->
+			<br>
+			<br>
+			<br>
+			</div>
+		</div>
+	</div>
+<!--===============================================================================================-->
+	<script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/bootstrap/js/popper.js"></script>
+	<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/daterangepicker/moment.min.js"></script>
+	<script src="../vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="../js/main.js"></script>
+<!-- On importe les fonctionalité de bootsrap néscéssaires au design de la page-->
+</body>
+</html>
