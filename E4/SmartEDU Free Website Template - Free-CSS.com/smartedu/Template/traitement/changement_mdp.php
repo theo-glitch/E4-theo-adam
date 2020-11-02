66d8b0f5ae088bdef51158f1136d5e06c9f790a9
theo-glitch <servatheo971@gmail.com>
2020-11-02 16:02:50 +0100

    fonction

--- E4/SmartEDU Free Website Template - Free-CSS.com/smartedu/Template/traitement/changement_mdp.php
+++ E4/SmartEDU Free Website Template - Free-CSS.com/smartedu/Template/traitement/changement_mdp.php
@@ -7,4 +7,4 @@ $modif = new User(['email'=>$_POST['Email']]);
 
 //Modification dans la bdd
 $update = new Manager_User;
-$update->modification($modif, $_SESSION['email']);
+$update->modification_mdp($modif, $_SESSION['email']);
